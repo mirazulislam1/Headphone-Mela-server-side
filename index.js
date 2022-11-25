758fa311415b9c62ad56a3dbeffafab826a3136b
unknown <you@example.com>
2022-11-25 20:53:57 +0600

    categories collection added

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+// Application code
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qrjo9vr.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// crud operation
+async function run(){
+    try{
+        const categoriesCollection = client.db('categoryProduct').collection('categories');
+
+        app.get('/categories', async(req, res)=>{
+            const query= {};
+            const cursor = categoriesCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(error => console.error(error))
 
 app.get('/', async(req, res)=>{
     res.send('Resell server side is running ')
