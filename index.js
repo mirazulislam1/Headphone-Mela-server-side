313d756fb1d192f10cc28ba10b50c7824738e888
unknown <you@example.com>
2022-11-29 11:49:30 +0600

    added booking buyer

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ async function run(){
             res.send(productList);
         });
 
+        app.get('/bookings', async(req,res) =>{
+            const email = req.query.email;
+            const query = {email: email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            console.log(bookings)
+            res.send(bookings);
+        })
+
         app.post('/bookings', async(req, res) =>{
             const bookings = req.body;
             const result = await bookingsCollection.insertOne(bookings);
